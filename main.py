48728dbeb88d6c6056c3ab2d771f274b6d4288d0
Carl Morrissey <carl.morrissey@gmail.com>
2016-04-28 00:49:28 -0400

    working model

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
 import json
-
+import re
+from slacker import Slacker
 
 f = open('token.json', 'r')
-print json.loads(f.readline())
+config = json.loads(f.read())
+token = config['token']
+channels = config['channels']
+f.close()
+
+ts = open('timestamp.dat', 'r')
+oldest_ = ts.readline()
+ts.close()
+
+slack = Slacker(token)
+
+latest = oldest_
+for channel_ in channels:
+    response = slack.channels.history(channel_, oldest=oldest_, inclusive=0)
+    messages = response.body['messages']
+    for message in messages:
+        if message['ts'] > latest:
+            latest = message['ts']
+        if re.search(r'(marki)|(plier)', message['text'], re.I):
+            slack.reactions.add('cold_sweat', channel=channel_, timestamp=message['ts'])
+
+ts = open('timestamp.dat', 'w')
+ts.write(latest)
+ts.close()
